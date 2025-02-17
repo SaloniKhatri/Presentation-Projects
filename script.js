fbda0cc17ad0bc3b4a9c58ed0e9a886dd685a492
SaloniKhatri <122539374+SaloniKhatri@users.noreply.github.com>
2025-02-17 20:31:31 +0530

    All JS Projects

---
+++ script.js
@@ -0,0 +1,73 @@
+
+function openLink1() {
+    const url = "https://salonikhatri.github.io/Random-quote-generator/"; 
+    
+    window.open(url, "_blank"); // Opens in a new tab
+}
+function openLink2() {
+    const url = "https://salonikhatri.github.io/Number-Guessing/"; 
+    
+    window.open(url, "_blank"); // Opens in a new tab
+}
+function openLink3() {
+    const url = "https://salonikhatri.github.io/temperature-converter/"; 
+    
+    window.open(url, "_blank"); // Opens in a new tab
+}
+function openLink4() {
+    const url = "https://salonikhatri.github.io/Info-Display/"; 
+    
+    window.open(url, "_blank"); // Opens in a new tab
+}
+function openLink5() {
+    const url = " https://salonikhatri.github.io/Background-color-changing/"; 
+    
+    window.open(url, "_blank"); // Opens in a new tab
+}
+function openLink6() {
+    const url = "https://salonikhatri.github.io/Form-validation/"; 
+    
+    window.open(url, "_blank"); // Opens in a new tab
+}
+function openLink7() {
+    const url = "https://salonikhatri.github.io/OTPGenerator/"; 
+    
+    window.open(url, "_blank"); // Opens in a new tab
+}
+// function openLink8() {
+//     const url = ""; 
+    
+//     window.open(url, "_blank"); // Opens in a new tab
+// }
+// function openLink9() {
+//     const url = ""; 
+    
+//     window.open(url, "_blank"); // Opens in a new tab
+// }
+
+const button1 = document.getElementById("button1");
+button1.onclick = openLink1;
+
+const button2 = document.getElementById("button2");
+button2.onclick = openLink2;
+
+const button3 = document.getElementById("button3");
+button3.onclick = openLink3;
+
+const button4 = document.getElementById("button4");
+button4.onclick = openLink4;
+
+const button5 = document.getElementById("button5");
+button5.onclick = openLink5;
+
+const button6 = document.getElementById("button6");
+button6.onclick = openLink6;
+
+const button7 = document.getElementById("button7");
+button7.onclick = openLink7;
+
+// const button8 = document.getElementById("button8");
+// button8.onclick = openLink8;
+
+// const button9 = document.getElementById("button9");
+// button9.onclick = openLink9;
